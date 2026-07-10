7eb0dcd5b525e0a88af4b8aea34c32658e056f3c
Nazywhyte <Nazysandy@gmail.com>
2026-07-10 10:06:49 -0400

    create a new dev vpc

--- main.tf
+++ main.tf
@@ -13,11 +13,45 @@ data "aws_ami" "app_ami" {
   }
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
 
   tags = {
-    Name = "HelloWorld"
+    Terraform = "true"
+    Environment = "dev"
   }
+}
+
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets [0]
+
+  tags = {
+    Name = "Learning Terraform"
+  }
+}
+
+module "blog_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id = module.blog_vpc.vpc_id
+  name = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
